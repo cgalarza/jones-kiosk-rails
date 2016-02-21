e29d072befa85fbd364905b3000f67c732d4cc88
Carla Galarza <carla.m.galarza.13@dartmouth.edu>
2016-02-21 12:50:38 -0500

    Added genre navbar

--- lib/sierra_api.rb
+++ lib/sierra_api.rb
@@ -46,6 +46,9 @@ class SierraApi
 
   # Get authentication token and save when this token will expire.
   def authenticate
+    raise 'Sierra api key not present' unless ENV['SIERRA_API_KEY']
+    raise 'Sierra api secret not present' unless ENV['SIERRA_API_SECRET']
+
     if authentication_expired?
       credentials = Base64.strict_encode64(ENV['SIERRA_API_KEY'] + ':' + ENV['SIERRA_API_SECRET'])
       # throw error if api key or secret key is missing.
