7c455b63ba9c543afa01e5c215557a11238fc262
Carla Galarza <carla.m.galarza.13@dartmouth.edu>
2016-02-22 21:40:10 -0500

    Cleaned up catalog query in preparation for keyword search page.

--- lib/catalog_query.rb
+++ lib/catalog_query.rb
@@ -2,35 +2,29 @@ require 'open-uri'
 require 'nokogiri'
 
 module CatalogQuery
+  KEYWORD_SEARCH_BEG = "http://libcat.dartmouth.edu/search/X?SEARCH="
+  KEYWORD_SEARCH_END = "+and+(branch%3Abranchbajmz+or+branch%3Abranchbajmv+or+branch%3Abranchrsjmc)&searchscope=4&SORT=R&Da=&Db=&p="
+
   GENRE_URL_BEG = 'http://libcat.dartmouth.edu/search/X?s:'.freeze
   GENRE_URL_END = '%20and%20branch:branchbajmz'.freeze
 
+  # Keyword query catalog for dvds at Jones.
+  #
+  # @param search_term [String]
+  # @return [Array<String>] where each string is a bibnumber
+  def keyword_query(search_term)
+    raise 'Missing search term' unless search_term
+
+    url = [KEYWORD_SEARCH_BEG, search_term.gsub(' ', '+'), KEYWORD_SEARCH_END].join('');
+    CatalogQuery.query(url)
+  end
+
   # Query for a genre and returns bibnumber of results.
   #
-  # @params genre [String] genre to be queried for
-  # @returns [Array<String>] where each string is a bibnumber
+  # @param genre [String] genre to be queried for
+  # @return [Array<String>] where each string is a bibnumber
   def self.genre_query(genre)
-    page = Nokogiri::HTML(open(genre_url(genre)))
-    bibnumber_nodes = page.xpath('//td[@class=\'briefcitActions\']/input/@value')
-
-    # If there is only one result the entire record is displayed, therefore the
-    # bibnumber is derived differently than if multiple results are shown.
-    if bibnumber_nodes.count == 0
-      'only one result'
-    else
-      bibnumber_nodes.map(&:value).each { |b| b[0] = '' }# remove 'b'
-    end
-
-    #returns results
-  end
-
-  def self.genre_url(genre)
-    urls = CatalogQuery.genre_urls
-    (urls.key? genre) ? urls[genre] : false;
-  end
-
-  def self.genre_urls
-    {
+    urls = {
       adventure: common_genre_url('adventure%20films'),
   		animation: common_genre_url('Animated%20films'),
   		children: "http://libcat.dartmouth.edu/search~S1?/Xs:children%27s%20films%20and%20branch:branchbajmz",
@@ -46,6 +40,27 @@ module CatalogQuery
   		war: common_genre_url("war%20films"),
   		western: common_genre_url("western*"),
     }
+    url = (urls.key? genre) ? urls[genre] : false;
+    CatalogQuery.query(url)
+  end
+
+  # Query library catalog using url given and return array of the first 50
+  # bibnumbers returned.
+  #
+  # @param url [String] catalog url of query
+  # @return [Array<String>] where each string is a bibnumber
+  def self.query(url)
+    page = Nokogiri::HTML(open(url))
+    bibnumber_nodes = page.xpath('//td[@class=\'briefcitActions\']/input/@value')
+
+    # If there is only one result the entire record is displayed, therefore the
+    # bibnumber is derived differently than if multiple results are shown.
+    if bibnumber_nodes.count == 0
+      'only one result'
+    else
+      bibnumber_nodes.map(&:value).each { |b| b[0] = '' }# remove 'b'
+    end
+    #returns results
   end
 
   private
